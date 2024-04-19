c3427cc2e3625249ddd8dd9e846f53970044b7eb
Williams Hernandez <hernandw@gmail.com>
2024-04-19 11:49:22 -0400

    :sparkles: test con mocha

--- router/router.js
+++ router/router.js
@@ -3,7 +3,19 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! con router');
+})
+
+router.get('/country', (req, res) => {
+    const pais = {
+        id: 1,
+        country: 'Colombia',
+        capital: 'Bogota',
+        population: 40000000,
+        year: 2021,
+        days: 17
+    }
+    res.json(pais)
 })
 
 export default router
--- test/prueba.test.js
+++ test/prueba.test.js
@@ -1,4 +1,12 @@
-import { expect } from "chai";
+import { expect, use } from "chai";
+import chaiHttp from "chai-http";
+
+const chai = use(chaiHttp);
+chai.request();
+
+/* import router from "../router/router.js"; */
+
+const url = "http://localhost:3000";
 
 describe("Primer encuentro con los test con Mocha", () => {
   it("Primer test con Mocha", () => {
@@ -8,17 +16,52 @@ describe("Primer encuentro con los test con Mocha", () => {
 
     expect(sum).to.equal(6);
   });
-    it.skip("Segundo test con Mocha que probara una multiplicacion", () => {
-        let num1 = 5;
-        let num2 = 10;
-        let resultado = num1 * num2;
-        expect(resultado).to.equal(50);
+  it("Segundo test con Mocha que probara una multiplicacion", () => {
+    let num1 = 5;
+    let num2 = 10;
+    let resultado = num1 * num2;
+    expect(resultado).to.equal(50);
+  });
+  it("Tercer test con Mocha que probara una division", () => {
+    let num1 = 10;
+    let num2 = 5;
+    let resultado = num1 / num2;
+    expect(resultado).to.equal(2);
+  });
+  it.skip("Cuarto test con Mocha que probara la ruta raiz", () => {
+    chai
+      .request(url)
+      .get("/")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal("Hello World!");
+      });
+  });
+  it("Quinto test con Mocha que probara la ruta /country", () => {
+    chai
+      .request(url)
+      .get("/country")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.deep.equal({
+          id: 1,
+          country: "Colombia",
+          capital: "Bogota",
+          population: 40000000,
+          year: 2021,
+          days: 17
+        });
+      });
+  })
+  it("Sexto test con Mocha que probara la ruta /country", () => {
+    chai
+      .request(url)
+      .get("/country")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("countries");
+        expect(res.body).to.have.property("year");
+        });
+      });
+  })
 
-    })
-    it("Tercer test con Mocha que probara una division", () => {
-        let num1 = 10;
-        let num2 = 5;
-        let resultado = num1 / num2;
-        expect(resultado).to.equal(2);
-    })
-});
